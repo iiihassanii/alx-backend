51f1c41a9b0a5f96b68cee694eed24945e70b320
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-26 12:04:28 +0300

    task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -29,12 +29,12 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
         if key in self.cache_data:
-            self.cache_data.move_to_end(key)  # Mark as recently used
+            self.cache_data.move_to_end(key)
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            # Remove the least recently used item
             first_key = list(self.cache_data.keys())[0]
             print('DISCARD: ' + str(first_key))
+            self.cache_data.pop(first_key)
 
     def get(self, key):
         """ return the value in self.cache_data linked to key
@@ -47,5 +47,6 @@ class LRUCache(BaseCaching):
         """
         if key is None:
             return None
-        self.cache_data.move_to_end(key)
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
         return self.cache_data.get(key)
