14b517c6b00d8cc1ad38abcb8546ce644bcc9ce9
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-26 12:28:35 +0300

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" FIFOCache module"""
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """MRUCache
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+
+    def __init__(self):
+        """__init__
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+          the item value for the key key
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = list(self.cache_data.keys())[-2]
+            print('DISCARD: ' + str(first_key))
+            self.cache_data.pop(first_key)
+
+    def get(self, key):
+        """ return the value in self.cache_data linked to key
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if key is None:
+            return None
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key)
