037c82da64ccab57376e01685dfb53a1e03f0d4a
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-26 11:06:34 +0300

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" 0-main """
+BaseCaching = __import__('BaseCaching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+
+    def __init__(self):
+        """_summary_
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key == None or item == None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """_summary_
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if key == None:
+            return None
+        return self.cache_data.get(key)
