b597385488a28a73dd73c85ae54fe957c13475f9
iiihassanii <hassanabdelazim.max@gmail.com>
2024-06-26 11:57:47 +0300

    task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" FIFOCache module"""
+
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """LRUCache
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+
+    def __init__(self):
+        """__init__
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data
+          the item value for the key key
+        Args:
+            key (_type_): _description_
+            item (_type_): _description_
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)  # Mark as recently used
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Remove the least recently used item
+            first_key = list(self.cache_data.keys())[0]
+            print('DISCARD: ' + str(first_key))
+
+    def get(self, key):
+        """ return the value in self.cache_data linked to key
+
+        Args:
+            key (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        if key is None:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data.get(key)
