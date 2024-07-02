5bd6300338e0d93097f6da6dd55d74aced91ab03
iiihassanii <hassanabdelazim.max@gmail.com>
2024-07-02 13:36:23 +0300

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,15 +4,19 @@
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def get_index() -> str:
-    """The home/index page.
+def index() -> str:
+    """_summary_
+
+    Returns:
+        _type_: _description_
     """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
